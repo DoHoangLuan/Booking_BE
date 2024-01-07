70df37ce86c9a1566020a717f5e72e64df17aaee
TanDung5532 <tandung55321@gmail.com>
2024-01-07 13:35:39 +0700

    update: auth feature

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import express from 'express';
 
 import { connectToDatabase } from './configs/db.config';
 import router from './route';
+import { errorHandlingMiddleware } from './middlewares/handleError.middleware';
 
 const app = express();
 const PORT = 3001;
@@ -14,6 +15,7 @@ connectToDatabase();
 app.use(express.json());
 app.use(cors());
 app.use('/api/v1', router);
+app.use(errorHandlingMiddleware); 
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
--- src/route/auth.route.js
+++ src/route/auth.route.js
@@ -1,94 +1,14 @@
-import bcrypt from 'bcrypt';
 import express from 'express';
-import jwt from 'jsonwebtoken';
 
+import { authController } from '@/controllers/auth.controller';
 import { authMiddleware } from '@/middlewares/auth.middlewares';
-import UserModel from '@/models/userModel.js';
-import { findUserByEmail, saveNewUser } from '@/service/authService';
 
 const routerAuth = express.Router();
 
-routerAuth.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    if (!email || !password) {
-      return res.status(400).json({
-        message: 'Missing required key',
-      });
-    }
-    const existingUser = await findUserByEmail(email);
-    if (!existingUser) {
-      return res.status(401).json({
-        message: 'User not found',
-      });
-    }
-    const isMatchPassword = await bcrypt.compare(password, existingUser.password);
-    if (!isMatchPassword) {
-      return res.status(401).json({
-        message: 'Wrong password',
-      });
-    }
-    const jwtPayLoad = {
-      username: existingUser.username,
-      id: existingUser.id,
-      email: existingUser.email,
-    };
-    const SECRET_KEY = process.env.SECRET_KEY || 'trinvm';
-    const token = jwt.sign(jwtPayLoad, SECRET_KEY, {
-      expiresIn: '1h',
-    });
-    return res.status(200).json({
-      accessToken: token,
-      message: 'Login Successfully',
-    });
-  } catch (error) {
-    console.log('🚀 ~ file: auth.route.js:50 ~ routerAuth.post ~ error:', error);
-    return res.status(500).json({
-      message: error,
-    });
-  }
-});
+routerAuth.post('/login', authController.login);
 
-routerAuth.post('/register', async (req, res) => {
-  try {
-    const { email, username, password } = req.body;
-    if ((!email || !username, !password)) {
-      return res.status(400).json({
-        status: 'Error',
-        message: 'Missing required key',
-      });
-    }
-    const existingUser = await findUserByEmail(email);
-    console.log('🚀 ~ file: auth.route.js:65 ~ routerAuth.post ~ existingUser:', existingUser);
-    if (existingUser) {
-      return res.status(400).json({
-        status: 'Fail',
-        message: 'User has a already',
-      });
-    }
+routerAuth.post('/register', authController.register);
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    await saveNewUser(email, username, hashedPassword);
-
-    return res.status(200).json({
-      status: 'Success',
-      message: 'Register success',
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: err,
-    });
-  }
-});
-
-routerAuth.get('/me', authMiddleware, async (req, res) => {
-  const { id } = req.users;
-  const currentUser = await UserModel.findById(id).select('-password');
-  res.status(200).json({
-    Userinfo: currentUser,
-  });
-});
+routerAuth.get('/me', authMiddleware, authController.info);
 
 export default routerAuth;
